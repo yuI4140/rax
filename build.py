f2092741f0dd111c50cac1e4fca31c928eb9d2f3
yuI4140 <greyberzambrano79@gmail.com>
2023-06-06 21:52:53 +0200

    fix add including in build

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ gcc -Wall -Wextra -Werror -I./headers -o ./bin/main ./src/main.c
 
 linux = """    
 gcc -Wall -Wextra -Werror -I./headers -o ./bin/main ./src/main.c
-x86_64-w64-mingw32-gcc -Wall -Wextra -Werror -I./headers o ./bin/main ./src/main.c
+x86_64-w64-mingw32-gcc -Wall -Wextra -Werror -I./headers -o ./bin/main ./src/main.c
 """
 
 os.system("touch build/build.sh")
