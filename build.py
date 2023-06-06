adc97bb9663d4410fa1005444a18bb3365434dc4
yuI4140 <greyberzambrano79@gmail.com>
2023-06-06 21:35:24 +0200

    init-fix-2

--- build.py
+++ build.py
@@ -1,14 +1,22 @@
 import os
+
 os.system("mkdir build bin src headers third-party")
 os.system("touch src/main.c")
-win="""
+
+win = """
 gcc -Wall -Wextra -Werror -o ../bin/main ../src/main.c
 """
-linux="""    
+
+linux = """    
 gcc -Wall -Wextra -Werror -o ../bin/main ../src/main.c
 x86_64-w64-mingw32-gcc -Wall -Wextra -Werror -o ../bin/main ../src/main.c
 """
+
 os.system("touch build/build.sh")
 os.system("touch build/build.bat")
-os.system("echo {}>build/build.sh".format(linux))
-os.system("echo {}>build/build.bat".format(win))
+
+with open("build/build.sh", "w") as sh_file:
+    sh_file.write(linux)
+
+with open("build/build.bat", "w") as bat_file:
+    bat_file.write(win)
