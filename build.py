90d0ca1509b94e5405e54d2098170229e1d38aff
yuI4140 <greyberzambrano79@gmail.com>
2023-06-21 13:54:53 +0200

    update-r5

--- build.py
+++ build.py
@@ -195,7 +195,7 @@ linux = """
 gcc -Wall -Wextra -Werror -I./headers -o ./bin/main ./src/main.c
 x86_64-w64-mingw32-gcc -Wall -Wextra -Werror -I./headers -o ./bin/main ./src/main.c
 """
-os.system("mv ../bool.h headers")
+os.system("mv ./buildProj/bool.h headers")
 with open("build/build.sh", "w") as sh_file:
     sh_file.write(linux)
 with open("headers/defines.h", "w") as file:
