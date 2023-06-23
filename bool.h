191ee8c08e8cfa1a09e68a8aa9551e058518b04c
yuI4140 <greyberzambrano79@gmail.com>
2023-06-23 09:41:14 +0200

    update-r7

--- bool.h
+++ bool.h
@@ -1,21 +1,23 @@
 #include <stdlib.h>
-struct bl {char value;};
+struct bl {
+  char value;
+};
 #ifdef BOOL_IMP
 typedef struct bl bool;
-bool newBool(char v){
-if(v>1 || v<0){
-   printf("ERROR: passed value is not bool --➤  NULL \n");
-   exit(1);
+bool newBool(char v) {
+  if (v > 1 || v < 0) {
+    printf("ERROR: passed value is not bool --➤  NULL \n");
+    exit(1);
+  }
+  bool ret = {v};
+  return ret;
 }
-   bool ret={v};
-   return ret;
-}
-bool checkBool(bool* bl){
-if(bl->value>1 || bl->value<0){
-   printf("ERROR: passed value is not bool --➤  NULL\n");
-   exit(1);
-}
-  bool newBl={bl->value};
+bool checkBool(bool *bl) {
+  if (bl->value > 1 || bl->value < 0) {
+    printf("ERROR: passed value is not bool --➤  NULL\n");
+    exit(1);
+  }
+  bool newBl = {bl->value};
   return newBl;
 }
 #endif
