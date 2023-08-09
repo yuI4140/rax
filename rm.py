c680e266f1b2d72d707416481cce9bd9cb68ac9d
yuI4140 <greyberzambrano79@gmail.com>
2023-08-09 17:03:12 +0200

    fixed syntax error of rm.py

--- rm.py
+++ rm.py
@@ -7,18 +7,18 @@ there is no fix to remove .git that doesn't be
 remove with sudo. If yoou have a better solution please
 make a issue.\n"""
 if platform.system()=="Linux": 
-    if argv[1]=="--nla":
+    if argc!=0 and argv[1]=="--nla":
         os.system("python3 ./buildproj/build.py")
         os.system("mv ./buildproj/bool.h ./headers")
         os.system("sudo rm -r ./buildproj/.git")
         os.system("sudo rm -r ./buildproj/") 
-else: 
-    print("Desable LINUX_ADV setting the flag:"+argv[0]+" --nla") 
-    print(LINUX_ADV)
-    os.system("python3 ./buildproj/build.py")
-    os.system("mv ./buildproj/bool.h ./headers")
-    os.system("sudo rm -r ./buildproj/.git")
-    os.system("sudo rm -r ./buildproj/") 
+    else: 
+        print("Desable LINUX_ADV setting the flag:"+argv[0]+" --nla") 
+        print(LINUX_ADV)
+        os.system("python3 ./buildproj/build.py")
+        os.system("mv ./buildproj/bool.h ./headers")
+        os.system("sudo rm -r ./buildproj/.git")
+        os.system("sudo rm -r ./buildproj/") 
 elif platform.system()=="Windows":
     os.system("py .\\buildproj\\build.py")
     files = glob.glob(".\\buildproj\\*[!py]")
