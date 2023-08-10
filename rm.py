8f0c4953dc0cba23ae0a6a4777d3c267e7f48b1e
yuI4140 <greyberzambrano79@gmail.com>
2023-08-10 22:28:20 +0200

    support Windows&Linux

--- rm.py
+++ rm.py
@@ -1,2 +1,14 @@
-import os
-os.system("python3 ./buildProj/build.py && rm -r ./buildProj/")
+import os,sys
+argv=sys.argv
+argc=len(argv)-1
+if argv[1]=="-h":
+    print(argv[0]+" linux // this activete the linux option")
+    print(argv[0]+"// this activete the Windows option")
+if argv[1]=="linux": 
+    os.system("python3 ./buildproj/build.py")
+    os.system("mv ./buildproj/bool.h ./headers")
+    os.system("sudo rm -r ./buildproj/.git")
+    os.system("sudo rm -r ./buildproj/)
+else:
+    os.system("del ./buildproj/.git")
+    os.system("del ./buildproj/)
