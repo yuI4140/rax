fe533bdcd7f78aad61ddb1198ed4c62c7bafa85b
yuI4140 <greyberzambrano79@gmail.com>
2023-08-11 03:45:38 +0200

    better build-system

--- rm.py
+++ rm.py
@@ -1,14 +1,11 @@
-import os,sys
-argv=sys.argv
-argc=len(argv)-1
-if argv[1]=="-h":
-    print(argv[0]+" linux // this activete the linux option")
-    print(argv[0]+"// this activete the Windows option")
-if argv[1]=="linux": 
+import os,platform
+if platform.system()=="Linux": 
     os.system("python3 ./buildproj/build.py")
     os.system("mv ./buildproj/bool.h ./headers")
     os.system("sudo rm -r ./buildproj/.git")
-    os.system("sudo rm -r ./buildproj/)
-else:
+    os.system("sudo rm -r ./buildproj/")
+elif platform.system()=="Windows":
     os.system("del ./buildproj/.git")
-    os.system("del ./buildproj/)
+    os.system("del ./buildproj/")
+else:
+    print(platform.system()+" not supported")
