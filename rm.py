d649caf7dc6611d9321a9a2b7dacc0030cd0a983
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-08-11 02:32:43 +0200

    Update rm.py

--- rm.py
+++ rm.py
@@ -1,11 +1,15 @@
-import os,platform
+import os,platform, glob
 if platform.system()=="Linux": 
     os.system("python3 ./buildproj/build.py")
     os.system("mv ./buildproj/bool.h ./headers")
     os.system("sudo rm -r ./buildproj/.git")
     os.system("sudo rm -r ./buildproj/")
 elif platform.system()=="Windows":
-    os.system("del ./buildproj/.git")
-    os.system("del ./buildproj/")
+    os.system("py .\\buildproj\\build.py")
+    files = glob.glob(".\\buildproj\\*[!py]")
+    for f in files:
+        os.rename(f, os.path.basename(f))
+    os.system("rmdir /s /q .\\buildproj")
+    os.system("move ./bool.h ./headers")
 else:
     print(platform.system()+" not supported")
